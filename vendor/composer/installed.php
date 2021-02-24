1d7817a1aa0cb4a5c8462d074283f11020c4fa5f
EricMartinezEgibide <eric.martinez@ikasle.egibide.org>
2021-02-24 11:36:02 +0100

    Solucionados fallos Eric

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,12 +1,12 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '7f6f98e998c436c302f4755f47f319cb9b130da8',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -440,12 +440,12 @@
     ),
     'laravel/laravel' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '7f6f98e998c436c302f4755f47f319cb9b130da8',
     ),
     'laravel/sail' => 
     array (
