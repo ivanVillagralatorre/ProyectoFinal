8a4598415fdc6623a2b843b1e0cc0f94209a7166
EricMartinezEgibide <eric.martinez@ikasle.egibide.org>
2021-02-24 13:39:00 +0100

    Home en progreso

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <link rel="icon" type="image/png" href="images/logo.png">
+</head>
+
+@include ("partials.topbar")
+
+@yield("content")
+
+@include ("partials.footer")
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view("/home", "home");
+Route::view("/layout", "layout");
