d8bb52f2358d4976fc4500b64f8932717c1f0426
Eric <ericalafuga@hotmail.com>
2021-02-24 18:17:57 +0100

    Menu principal y perfil de usuario estructurado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,50 @@
 
     <div class="container-fluid d-flex flex-column">
 
+        <br>
+
         <div>
-            <p class="h4">Crear proyecto</p>
-            <button type="button" class="btn btn-dark">Dark</button>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#CrearProyecto">
+                Crear un nuevo proyecto
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="CrearProyecto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Crear proyecto</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Título del proyecto</label>
+                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mi proyecto">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Descripción</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                            <button type="button" class="btn btn-dark">Crear</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+        <br>
 
         <!-- TABLA CON LOS PROYECTOS -->
         <div>
@@ -18,30 +58,43 @@
                 <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Fecha de inicio</th>
+                    <th scope="col">Número de participantes</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
                     <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>Mi proyecto</td>
+                    <td>05/02/2021</td>
+                    <td>5</td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>Gestor de empleados</td>
+                    <td>13/01/2021</td>
+                    <td>13</td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
+                    <td>El proyecto final</td>
+                    <td>26/01/2021</td>
+                    <td>2</td>
                 </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Ayuntamiento vitoria</td>
+                    <td>22/01/2021</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Diccionario digital</td>
+                    <td>25/02/2021</td>
+                    <td>4</td>
+                </tr>
+
                 </tbody>
             </table>
         </div>
--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -4,40 +4,64 @@
 </head>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img style="max-height: 40px" src="images/logo.png">
+    </a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
+    <a class="navbar-brand" href="#" data-toggle="modal" data-target="#perfil">
+        <img style="max-height: 40px" src="images/userDefault.png">
+    </a>
 
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <li class="nav-item dropdown" style="list-style-type: none;">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img style="max-height: 40px" src="images/userDefault.png">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </form>
-    </div>
 </nav>
+
+<!--PERFIL-->
+
+<div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="perfil" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Datos de tu perfil</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Nombre</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre" disabled>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Apellidos</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Apellido" disabled>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Email</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" disabled>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Contraseña</label>
+                            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="*******" disabled>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Editar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
