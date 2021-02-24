963324e5c294cb52046cfb6c519562ec88f17283
ivan <ivan.villagra@ikasle.egibide.org>
2021-02-24 13:43:49 +0100

    rutas e imagenes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 <body>
 <div class="container-fluid">
     <div class="row no-gutter">
-        <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+        <div class="d-none d-md-flex col-md-4 col-lg-6"> <img src=""></div>
         <div class="col-md-8 col-lg-6">
             <div class="login d-flex align-items-center py-5">
                 <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-})->name('index');
+Route::get('/',"LoginView@index")->name('index');
 
 Auth::routes();
 
