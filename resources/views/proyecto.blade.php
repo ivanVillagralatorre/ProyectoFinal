bc6d7eefa299b9329bae4582a07dec21ffc137f5
ivan <ivan.villagra@ikasle.egibide.org>
2021-02-26 10:23:29 +0100

    plantilla maquetacion comentarios

--- resources/views/proyecto.blade.php
+++ resources/views/proyecto.blade.php
@@ -5,11 +5,13 @@
 @section('content')
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i>
+        </button>
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search"
+                       aria-describedby="basic-addon2"/>
                 <div class="input-group-append">
                     <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
                 </div>
@@ -18,7 +20,8 @@
         <!-- Navbar-->
         <ul class="navbar-nav ml-auto ml-md-0">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                     <a class="dropdown-item" href="#">Settings</a>
                     <a class="dropdown-item" href="#">Activity Log</a>
@@ -39,40 +42,50 @@
                             Dashboard
                         </a>
                         <div class="sb-sidenav-menu-heading">Interface</div>
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts"
+                           aria-expanded="false" aria-controls="collapseLayouts">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             Layouts
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                             data-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
                                 <a class="nav-link" href="layout-static.html">Static Navigation</a>
                                 <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
                             </nav>
                         </div>
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                           aria-expanded="false" aria-controls="collapsePages">
                             <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                             Pages
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
+                             data-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                <a class="nav-link collapsed" href="#" data-toggle="collapse"
+                                   data-target="#pagesCollapseAuth" aria-expanded="false"
+                                   aria-controls="pagesCollapseAuth">
                                     Authentication
                                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                 </a>
-                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
+                                     data-parent="#sidenavAccordionPages">
                                     <nav class="sb-sidenav-menu-nested nav">
                                         <a class="nav-link" href="login.html">Login</a>
                                         <a class="nav-link" href="register.html">Register</a>
                                         <a class="nav-link" href="password.html">Forgot Password</a>
                                     </nav>
                                 </div>
-                                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                                <a class="nav-link collapsed" href="#" data-toggle="collapse"
+                                   data-target="#pagesCollapseError" aria-expanded="false"
+                                   aria-controls="pagesCollapseError">
                                     Error
                                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                 </a>
-                                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
+                                     data-parent="#sidenavAccordionPages">
                                     <nav class="sb-sidenav-menu-nested nav">
                                         <a class="nav-link" href="401.html">401 Page</a>
                                         <a class="nav-link" href="404.html">404 Page</a>
@@ -99,20 +112,21 @@
             </nav>
         </div>
         <div id="layoutSidenav_content">
-            <main class= " container-fluid bg-light p-4  ">
-                <div class="row d-flex flex-colum " >
-                <div class=" col-12 col-md-4 order-0 mt-md-4  order-md-1 bg-dark h-auto d-flex h-50 flex-column rounded order-0 order-md-1 align-items-center">
+            <main class="  bg-light d-flex flex-column flex-md-row h-100  ">
+
+                <div id="descripcion"class="  order-0 mt-md-4 flow-md-grow-5  order-md-1 bg-dark  d-flex  flex-column rounded order-0 order-md-1 align-items-center">
 
 
-                <h1 class="mb-3">Titulo</h1>
+                    <h1 class="mb-3">Titulo</h1>
 
-                <div class="col-10 col-md-4 bg-light rounded-0">
-                    <h2 class="align-self-start">Descripcion:</h2>
+                    <div class="bg-light rounded-0 ">
+                        <h2 class="align-self-start">Descripcion:</h2>
 
-                </div>
+                    </div>
                     <div class="d-flex justify-content-center ">
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-outline-danger mt-4 mb-3" data-toggle="modal" data-target="#myModal">
+                        <button type="button" class="btn btn-outline-danger mt-4 mb-3" data-toggle="modal"
+                                data-target="#myModal">
                             Añadir Participantes
                         </button>
                         <!-- Modal -->
@@ -130,7 +144,8 @@
                                         <p>Some text in the modal.</p>
                                     </div>
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close
+                                        </button>
                                     </div>
                                 </div>
 
@@ -140,64 +155,54 @@
                 </div>
 
 
-                <div  id='over'class="bg-light col-12 col-md-8 order-1 order-md-0 p-2 border border-4 rounded   mt-4 d-flex flex-column align-items-center  ">
+                <div id='over' class="bg-light order-1 order-md-0 p-2 border border-4 rounded   mt-4 d-flex flex-column   ">
+
+                    <div id="over2" class="d-flex flex-column align-items-center overflow-auto border">
 
 
+                        <div>a</div>
+                        <div>a</div>
 
-                      <div>a</div>
-                      <div>a</div>
+                        <div>a</div>
 
-                      <div>a</div>
-
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                      <div>a</div>
-                    <div>a</div>
-                    <div>a</div>
-                    <div>a</div>
-                    <div>a</div>
-                    <div>a</div>
-
-
-
-                </div>
-
-                    <div class="col-12 col-md-8 bg-secondary rounded order-2 ">
-
-
-                    <form class=>
-                        <div class=" row d-flex flex-column  ">
-                            <textarea  placeholder="Escribe tu mensaje"  class="rounded align-self-center mb-3 mt-4 overflow-hidden col-10 col-md-10  "></textarea >
-                            <button type="button" class="btn btn-lg col-10 align-self-center btn-dark">Enviar mensaje</button>
-                        </div>
-
-
-                    </form>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
+                        <div>a</div>
                     </div>
+
+                    <div class=" bg-secondary rounded order-2 border  ">
+
+
+                        <form class="d-flex flex-column">
+                            <textarea placeholder="Escribe tu mensaje"
+                                      class="rounded align-self-center  mt-4 overflow-hidden "></textarea>
+                            <button type="button" class="btn btn-lg mb-3 align-self-center btn-dark">Enviar
+                                mensaje
+                            </button>
+
+
+                        </form>
+                    </div>
+
                 </div>
+
 
             </main>
-            <footer class="py-4 bg-light mt-auto order-4 ">
-                <div class="container-fluid">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+
         </div>
     </div>
 
