0d264f6d892bb08fd91f0117b97ace990063106e
ivan <ivan.villagra@ikasle.egibide.org>
2021-02-28 13:18:37 +0100

    tareas basico realizado

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tarea;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TareasController extends Controller
 {
@@ -11,15 +14,32 @@ class TareasController extends Controller
     public function index()
     {
 
+        $listaTareas = Tarea::get();
 
-        return view('tareas');
+        
+
+        return view('tareas')->with('listatareas',$listaTareas);
     }
 
 
     public function store()
     {
+        \request()->validate([
+           'titulo'=>'required',
+            'descripcion'=>'required',
+
+        ]);
 
 
-        return request();
+        Tarea::Create([
+            'titulo'=>request('titulo'),
+            'descripcion'=>request('descripcion'),
+            'proyecto_id'=>\request('idP'),
+            'usuario_id'=> Auth::user()->id,
+            'estado'=>false
+
+        ]);
+
+        return redirect()->route('mostrarTareas');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post("/insertarProyecto", "ProyectoController@store")->name("insertarProy
 Route::get("/proyecto/{id}", "ProyectoController@show")->name("abrirProyecto");
 Route::post('/crearCom','ProyectoController@crearComentario')->name('crearComentario');
 Route::get('/tareas','TareasController@index')->name('mostrarTareas');
-Route::post('/tareas','TareasController@store')->name('crearTareas');
+Route::post('/crearTareas','TareasController@store')->name('crearTareas');
 
 
 //UsuariosProyectos
