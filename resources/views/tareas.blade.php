5b2e8fc9665aea29e72dc92d7d4d92e3718efcc8
ivan <ivan.villagra@ikasle.egibide.org>
2021-03-01 13:06:53 +0100

    tareas css mejorado

--- resources/views/tareas.blade.php
+++ resources/views/tareas.blade.php
@@ -51,7 +51,8 @@
 
       <div  id="contenedorTareas" class="d-flex justify-content-center flex-wrap w-100">
           @forelse($listatareas as $tarea)
-              <div class="accordion-item tstyle rounded id="tarea{{$tarea->id}}">
+              @if($tarea->estado==0)
+              <div class="accordion-item tstyle tarea rounded" style="background:#ffde69" id="tarea{{$tarea->id}}">
                   <h2 class="accordion-header" >
                       <button class=" text-center accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#t{{$tarea->id}}" aria-expanded="false" aria-controls="t{{$tarea->id}}">
                           <p class=" text-uppercase">{{$tarea->titulo}}</p>
@@ -63,12 +64,12 @@
                           <div class="w-50">
                               @if($tarea->estado==0)
                                   <div class="">
-                                      <p><strong>Estado</strong>:</p><span class="text-success">En procesp</span>
+                                      <p><strong>Estado</strong>:<strong class="text-success">En procesp</strong></p>
                                   </div>
 
                               @else
                                   <div class="">
-                                      <p class=>Estado: <span class="text-danger">Finalizada</span></p>
+                                      <p class=>Estado: <strong class="text-danger">Finalizada</strong></p>
                                   </div>
                               @endif
 
@@ -79,7 +80,7 @@
 
 
 
-                          <div class="w-50 d-flex justify-content-center flex-column">
+                          <div class="w-50 d-flex align-items-center flex-column">
                                   <p>Autor: {{$tarea->datosAutor['name']}}</p>
                               @if($tarea->estado==0)
                                   <P>Fecha-fin:<span>....</span></P>
@@ -134,7 +135,7 @@
                                   @if(auth()->user()->id==$tarea->datosAutor['id'])
 
                                   <button type="button"  class="btn w-75 btn-dark" data-toggle="modal" data-target="#modal{{$tarea->id}}">
-                                      Añadir Participante
+                                      Añadir
                                   </button>
 
                               @endif
@@ -171,14 +172,19 @@
                                   </div>
 
                                   @if($tarea->estado==0)
+
+                                 @endif
+                              </div>
+
+                          <div class="w-50">
                               <form action="{{route('endTarea')}}" method="POST">
                                   @csrf
                                   <input type="hidden" name="idT" value="{{$tarea->id}}">
                                   <button  class="btn  ml-1 btn-danger">finalizar</button>
 
                               </form>
-                                 @endif
-                              </div>
+
+                          </div>
 
 
                       </div>
@@ -186,6 +192,143 @@
 
                   </div>
               </div>
+            @else
+          <div class="accordion-item tstyle   rounded" style="background: lightgreen" id="tarea{{$tarea->id}}">
+          <h2 class="accordion-header" >
+              <button class=" text-center accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#t{{$tarea->id}}" aria-expanded="false" aria-controls="t{{$tarea->id}}">
+                  <p class=" text-uppercase">{{$tarea->titulo}}</p>
+              </button>
+          </h2>
+          <div id="t{{$tarea->id}}" class="accordion-collapse  rounded collapse" aria-labelledby="tarea{{$tarea->id}}" data-bs-parent="#contenedorTareas">
+              <div class="accordion-body w-100 d-flex flex-wrap p-2">
+
+                  <div class="w-50">
+                      @if($tarea->estado==0)
+                          <div class="">
+                              <p><strong>Estado</strong>:<strong class="text-success">En proceso</strong></p>
+                          </div>
+
+                      @else
+                          <div class="">
+                              <p class=><strong>Estado:</strong> <strong class="text-danger">Finalizada</strong></p>
+                          </div>
+                      @endif
+
+                      <P class="mb-1"><strong>Fecha_inicio</strong> </P>
+                      <span class="mt-0">{{$tarea->created_at}}</span>
+
+                  </div>
+
+
+
+                  <div class="w-50 d-flex align-items-center flex-column">
+                      <p><strong>Autor:</strong>  {{$tarea->datosAutor['name']}}</p>
+                      @if($tarea->estado==0)
+                          <P><strong>Fecha_fin:</strong> <span>....</span></P>
+                      @else
+                          <P class="mb-1"><strong>Fecha_fin:</strong></P>
+                          <span class="mt-0">{{$tarea->fecha_vencimiento}}</span>
+
+                      @endif
+                  </div>
+
+
+                  <div class="w-100 mt-3 mb-3">
+                      <P class="mb-1"><strong>Descripcion:</strong> </P>
+                      <span class="mt-0 mb-4 border-dark rounded">{{$tarea->descripcion}}</span>
+
+                  </div>
+
+
+                  <div class="w-50 justify-content-center p-1">
+                      <!-- Button trigger modal -->
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#listapart{{$tarea->id}}">
+                          Participantes
+                      </button>
+
+                      <!-- Modal -->
+                      <div class="modal fade" id="listapart{{$tarea->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                  <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLabel">Lista de participantes de la tarea</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                      </button>
+                                  </div>
+                                  <div class="modal-body">
+                                      @foreach($tarea->participantes as $participante)
+                                          <ul>
+                                              <li>{{$participante->name}}</li>
+                                          </ul>
+                                      @endforeach
+                                  </div>
+                                  <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+                  <div class="w-50 d-flex justify-content-center p-1">
+                      <!-- Button trigger modal -->
+                      @if(auth()->user()->id==$tarea->datosAutor['id'])
+
+                          <button type="button"  class="btn w-75 btn-dark" data-toggle="modal" data-target="#modal{{$tarea->id}}">
+                              Añadir
+                          </button>
+
+                      @endif
+
+                  <!-- Modal -->
+                      <div class="modal fade" id="modal{{$tarea->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                              <div class="modal-content">
+                                  <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLabel">Añadir participante a la tarea {{$tarea->id}}</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                      </button>
+                                  </div>
+
+
+                                  <form  method="post" action="{{route('addPtarea')}}">
+                                      @csrf
+                                      <div class="modal-body">
+                                          <!--FORMULARIO PARA LA CREACIÓN DE PROYECTOS-->
+                                          <div class="form-group d-flex">
+                                              <input type="hidden" value="{{$tarea->id}}" name="idT">
+                                              <input type="email" name="email" class="form-control"  placeholder="antonio@siwo.com">
+                                              <input type="submit" class="btn btn-dark ml-2" value="Añadir">
+                                          </div>
+                                      </div>
+                                      <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                      </div>
+                                  </form>
+
+                              </div>
+                          </div>
+                      </div>
+
+                      @if($tarea->estado==0)
+                          <form action="{{route('endTarea')}}" method="POST">
+                              @csrf
+                              <input type="hidden" name="idT" value="{{$tarea->id}}">
+                              <button  class="btn  ml-1 btn-danger">finalizar</button>
+
+                          </form>
+                      @endif
+                  </div>
+
+
+              </div>
+
+
+          </div>
+          </div>
+           @endif
 
       @empty
 
