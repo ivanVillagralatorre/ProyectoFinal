f799b198972b61d71920159acb80a42ef64e0278
ivan <ivan.villagra@ikasle.egibide.org>
2021-03-02 09:13:34 +0100

    margenes

--- resources/views/tareas.blade.php
+++ resources/views/tareas.blade.php
@@ -353,7 +353,7 @@
 
 
       </div>
-      <div class="d-flex w-100 justify-content-end border-bottom border-dark">
+      <div class="d-flex w-100 mb-4  mt-4 justify-content-end border-bottom border-dark">
 
           {{ $listatareas->links('pagination::bootstrap-4') }}
 
